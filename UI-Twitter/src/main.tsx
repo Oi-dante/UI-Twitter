412f2de379569440351fb0975a6839bc6507e182
Gustavo Barboza <gustavobarboza2003@gmail.com>
2023-04-05 20:59:00 -0300

    sideBar

--- UI-Twitter/src/main.tsx
+++ UI-Twitter/src/main.tsx
@@ -1,17 +1,91 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Hash, House, Bell, Envelope, BookmarkSimple, FileText, User, DotsThreeCircle, Sparkle } from 'phosphor-react'
 
 import './global.css'
+import twitterLogo from './assets/logo-twitter.svg'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div className="layout">
-      <aside className="sidebar">
-        sidebar
+      <aside className="side-bar">
+        <img className='logo' src={twitterLogo} alt="logo" />
+
+        <nav className='main-navigation'>
+
+          <a className='active' href="">
+            <House weight='fill'/>
+            Home
+          </a>
+
+          <a href="">
+            <Hash/>
+            Explore
+          </a>
+
+
+          <a href="">
+            <Bell/>
+            Notifications
+          </a>
+
+          <a href="">
+            <Envelope/>
+
+            Messages
+          </a>
+
+          <a href="">
+            <BookmarkSimple/>
+            Bookmarks
+          </a>
+
+          <a href="">
+            <FileText/>
+            Lists
+          </a>
+
+          <a href="">
+            <User/>
+            Profile
+          </a>
+
+          <a href="">
+            <DotsThreeCircle/>
+            More
+          </a>
+
+
+        </nav>
+
+        <button className='new-tweet'>Tweet</button>
+
       </aside>
+
+
+
       <div className="content">
-        
+        <main className='timeList'>
+          <div className='time-line-header'>
+            Home
+            <Sparkle/>
+          </div>
+
+          <form className='new-tweet-form'>
+            <label htmlFor="tweet">
+              <img src="https://github.com/oi-dante.png" alt="Gustavo Barboza" />
+              <textarea id="tweet" placeholder="What's happening?"/>
+            </label>
+
+            <button type='submit'>Tweet</button>
+          </form>
+
+        </main>
       </div>
+
+
+
+
     </div>
     
 
