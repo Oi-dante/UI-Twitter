c579681d811e66527bc72e1609617d54f6050718
Gustavo Barboza <gustavobarboza2003@gmail.com>
2023-04-13 21:20:08 -0300

    save-Tweets

--- UI-Twitter/src/pages/Timeline.tsx
+++ UI-Twitter/src/pages/Timeline.tsx
@@ -1,36 +1,54 @@
+import { FormEvent, useState } from "react"
 import { Header } from "../components/Header"
 import { Separator } from "../components/Separator"
 import { Tweet } from "../components/Tweets"
 
 import './Timeline.css'
-const tweets = [
+
+
+let newTweet = ''
+
+export function TimeLine() {
+
+  const [tweets, setTweets] = useState([
     'meu primeiro tweet',
     'teste',
     'Deu certo',
-]
+  ]
+  )
 
+  function createNewTweet(event: FormEvent){
+    event.preventDefault()
 
-export function TimeLine() {
-    return(
-        <main className='timeList'>
-          <Header title="Home" />
-         
+    setTweets([newTweet, ...tweets])
+  }
 
-          <form className='new-tweet-form'>
-            <label htmlFor="tweet">
-              <img src="https://github.com/oi-dante.png" alt="Gustavo Barboza" />
-              <textarea id="tweet" placeholder="What's happening?"/>
-            </label>
+  return(
+      <main className='timeList'>
+        <Header title="Home" />
+        
 
-            <button type='submit'>Tweet</button>
-          </form>
+        <form onSubmit={createNewTweet} className='new-tweet-form'>
+          <label htmlFor="tweet">
+            <img src="https://github.com/oi-dante.png" alt="Gustavo Barboza" />
+            <textarea 
+              id="tweet"
+              placeholder="What's happening?"
+              onChange={(event) => {
+                newTweet = event.target.value
+              }}
+             />
+          </label>
 
-          <Separator/> 
+          <button type='submit'>Tweet</button>
+        </form>
 
-          {tweets.map(tweet => {
-            return <Tweet key={tweet} content={tweet} />
-          })}
+        <Separator/> 
 
-        </main>
-    )
+        {tweets.map(tweet => {
+          return <Tweet key={tweet} content={tweet} />
+        })}
+
+      </main>
+  )
 }
